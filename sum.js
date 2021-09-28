c912b3506d217fbaf8a6a82f43b5781802331c29
eddc125 <eddclearning@gmail.com>
2021-09-28 14:49:56 +0800

    sum.js v2.0

--- sum.js
+++ sum.js
@@ -2,10 +2,12 @@ console.log("hello world");
 
 function sum(a) {
   let result = 0;
-  for (i = 0; i <= a; i++) {
+  for (let i = 0; i <= a; i++) {
     result = result + i;
   }
   console.log(result);
 }
 
-sum(6);
+sum(6); //21
+sum(10); //55
+sum(15); //120
