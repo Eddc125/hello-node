94e858b4ee1e1fb10949259456c5c8144f968263
eddc125 <eddclearning@gmail.com>
2021-10-16 11:17:06 +0800

    2021/10/16

---
+++ promise/readfile.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+// callback 版
+// fs.readFile('input.txt',"utf8",(err,data)=>{
+//     if(err){
+//         console.error('錯誤訊息',err)
+//     }else{
+//         console.log('正確讀到',data)
+//     }
+// })
+// promise 版
+let p = new Promise((resolve,reject)=>{
+    fs.readFile('input.txt',"utf8",(err,data)=>{
+        if(err){
+            reject(err);
+        }else{
+            resolve(data);
+        }
+    })
+});
+
+p.then((data)=>{
+    console.log("讀檔正確",data);
+}).catch((err) =>{
+    console.log("讀檔失敗",err);
+});
