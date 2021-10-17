b3d75328bd53bbed62eeac02db7e7c93df0725b5
eddc125 <eddclearning@gmail.com>
2021-10-17 17:05:50 +0800

    2021/10/17 - DB , delete

--- crawler/stock-await.js
+++ crawler/stock-await.js
@@ -1,24 +1,33 @@
-const axios = require('axios') ;
-
-let format = 'json';
-let today = '20211015';
-let stockCode = '0050';
+const axios = require("axios");
+const moment = require("moment"); // 先 npm install moment => require moment 就可以使用
+const fs = require("fs/promises");
 
 // await 版
-async function stocksSearch(format,today,stockCode){
+async function stocksSearch() {
+  // 通常會做排程工作設定自動執行的時間
+  // crontab、工作排程器....
+  let format = "json";
+  let today = moment().format("YYYYMMDD"); // .format('格式') 就會自動抓今天的日期
+  // let stockCode = "0050";
   try {
-    let res = await axios.get("https://www.twse.com.tw/exchangeReport/STOCK_DAY", {
-      params: {
-        response: format,
-        date: today,
-        stockNo: stockCode,
-      },
-    });
-    console.log(res.data);
-  }
-  catch(error){
-    console.error('錯誤',error);
+    let stockCode = await fs.readFile('stock.txt','utf-8');
+    // console.log(stockCode);
+    let res = await axios.get(
+      "https://www.twse.com.tw/exchangeReport/STOCK_DAY",
+      {
+        params: {
+          response: format,
+          date: today,
+          stockNo: stockCode,
+        },
+      }
+    );
+    //console.log(res.data);
+    let firstItem = res.data.data[0];
+    console.log(firstItem);
+  } catch (error) {
+    console.error("錯誤", error);
   }
 }
 
-stocksSearch(format,today,stockCode);
+stocksSearch();
